094e956aabafc466be79c65b92a00cec9e9ebe20
lokihardt <nine.twelve@foxmail.com>
2017-11-20 19:44:36 +0800

    Update pwn_quals.py

--- pwn/pwn_quals.py
+++ pwn/pwn_quals.py
@@ -5,55 +5,18 @@ import os, sys
 
 # switches
 DEBUG = 1
-
+BINARY_NAME = './tea'
 # modify this
-elf = ELF('./tea')
+elf = ELF(BINARY_NAME)
 
 if DEBUG:
-    io = process('./tea')
+    io = process(BINARY_NAME)
 else:
     io = remote(sys.argv[1], int(sys.argv[2]))
 
 if DEBUG: context(log_level='debug')
+pause()
+
 # define symbols and offsets here
 
-# simplified r/s function
-def ru(delim):
-    return io.recvuntil(delim)
 
-def rn(count):
-    return io.recvn(count)
-
-def ra(count):      # recv all
-    buf = ''
-    while count:
-        tmp = io.recvn(count)
-        buf += tmp
-        count -= len(tmp)
-    return buf
-
-def sl(data):
-    return io.sendline(data)
-
-def sn(data):
-    return io.send(data)
-
-def info(string):
-    return log.info(string)
-
-def dehex(s):
-    return s.replace(' ','').decode('hex')
-
-# define interactive functions here
-
-# define exploit function here
-def pwn():
-	
-    io.interactive()
-    return
-
-if __name__ == '__main__':
-    
-    pause()
-    pwn()
-    
