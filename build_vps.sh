bf3ce1d26b59e4173cb01f54df480be837f77492
lokihardt <nine.twelve@foxmail.com>
2020-11-20 09:39:54 +0800

    Update build_vps.sh

--- build_vps.sh
+++ build_vps.sh
@@ -2,5 +2,5 @@
 # commands to execute when you are building a new vps
 
 sudo apt update
-sudo apt install -y tmux gcc git zsh wget curl docker.io
+sudo apt install -y tmux gcc git zsh wget curl docker.io vim
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
