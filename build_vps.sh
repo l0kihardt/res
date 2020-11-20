763f2f0115d2dddb07f121e7fbb136a0aca6cc79
lokihardt <nine.twelve@foxmail.com>
2020-11-20 09:36:39 +0800

    Create build_vps.sh

---
+++ build_vps.sh
@@ -0,0 +1,6 @@
+#/bin/sh -e
+# commands to execute when you are building a new vps
+
+sudo apt update
+sudo apt install -y tmux gcc git zsh wget curl docker.io
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
